abe4eee0d629402ae1720f030ace06c92d3acfa4
a5822125 <a5822125@aoyama.jp>
2024-10-13 16:12:28 +0900

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { Providers } from './providers'
 
 export default function RootLayout({
@@ -7,6 +8,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <title>mail generator</title>
+        <meta name="description" content="簡単にメールを生成できるアプリケーションです。" />
+      </Head>
       <body>
         <Providers>{children}</Providers>
       </body>
