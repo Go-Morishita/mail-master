ab7d5ba4e320a64f27ce892a888bc20fb6080e54
a5822125 <a5822125@aoyama.jp>
2024-10-13 23:42:45 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { Providers } from './providers'
 
 export const metadata = {
   title: "mail-generator",
-  description: "mail-generator は、入力された情報をもとにAIが自動でメールを生成するツールです。わずか数クリックでメールを作成し、ビジネスや日常のやり取りを効率化します。\nPowered by ChatGPT-4o.\n© 2024 Go Morishita. All Rights Reserved.",
+  description: "面倒なメール作成は「mail-generator」にお任せを。\nPowered by ChatGPT-4o.\n© 2024 Go Morishita. All Rights Reserved.",
 }
 
 export default function RootLayout({
